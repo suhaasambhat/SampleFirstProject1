8783f9bbcaaad3557d344405a42795b73141636f
suhaasambhat <suhas.2909@gmail.com>
2023-05-09 16:43:28 +0530

    Initial commit

---
+++ src/sample/one/TestClass.java
@@ -0,0 +1,17 @@
+package sample.one;
+
+public class TestClass {
+    /**
+     * Documentational comment
+     * @param args
+     */
+    public static void main(String[] args){
+/*
+  Checking out
+        Multiline comment
+ */
+
+//        This is one comment
+        System.out.println("Hello World!!!");
+    }
+}
